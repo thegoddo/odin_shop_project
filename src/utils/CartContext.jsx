778bb1414873b6b9a1c3667b6ea9818645c6d364
theGoddo <78879232+thegoddo@users.noreply.github.com>
2024-09-15 11:50:36 +0530

    Created a Context for Cart.

---
+++ src/utils/CartContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useState } from "react";
+import PropTypes from "prop-types";
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => {
+    setCartItems((prevItems) => [...prevItems, item]);
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
+  };
+
+  const clearCart = () => {
+    setCartItems([]);
+  };
+
+  const getCartTotal = () => {
+    return cartItems.reduce((total, item) => total + item.price, 0);
+  };
+
+  const contextValue = {
+    cartItems,
+    addToCart,
+    removeFromCart,
+    clearCart,
+    getCartTotal,
+  };
+
+  return (
+    <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
+  );
+};
+
+CartProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default CartContext;
