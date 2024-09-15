5e7b83ebbf1c779cc97ecdc9ff2bde4b1449ffa6
theGoddo <78879232+thegoddo@users.noreply.github.com>
2024-09-15 11:52:03 +0530

    included cartprovider to main file.

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
 import "./index.css";
+import { CartProvider } from "./utils/CartContext";
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
   </StrictMode>
 );
